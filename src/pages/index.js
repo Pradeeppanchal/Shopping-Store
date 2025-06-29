5125d096c42d68033a00183a805738e9a6cc73a2
Pradeep Panchal <pradeeppanchal748@gmail.com>
2025-06-29 16:42:13 +0530

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Home from "./Home/Home";
+import Products from "./Products/Products";
+import Trands from "./Trands/Trands";
+import About from "./About/About";
+import Contact from "./Contact/Contact";
+import Sign from "./Sign/Sign";
+import Login from "./Login/Login";
+
+export {
+    Home,
+    Products,
+    Trands,
+    About,
+    Contact,
+    Sign,
+    Login,
+ };
